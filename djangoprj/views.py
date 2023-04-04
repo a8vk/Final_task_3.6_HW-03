5ec8b09ca0cc53e2b9b63ad1dfd4176501153340
a8vk <a8vk@ya.ru>
2023-04-04 20:19:38 +1000

    chek templates

--- djangoprj/views.py
+++ djangoprj/views.py
@@ -4,4 +4,4 @@ from djangoprj.models import Good
 
 def index(request):
     goods = Good.objects.all()
-    return render(request, 'index.html', context={'goods': goods})
+    return render(request, 'index.html', context={'goods': goods, 'name': 'Kia Rio 3'})
